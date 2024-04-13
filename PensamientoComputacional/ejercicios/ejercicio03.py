fc0b61feb9950ae310117a031b8b3fa6730648df
Nicolas <arganaraznicolas68@gmail.com>
2024-04-13 14:32:44 -0300

    add ejercicios clase 3

---
+++ PensamientoComputacional/ejercicios/ejercicio03.py
@@ -0,0 +1,10 @@
+def simulacion(cantidadPersonas):
+    return "resultados" 
+
+simulacion(6)
+
+'''
+1. Hacer un programa que simule el llenado de un álbum dinámicamente:
+Se asigna la tarea de crear un programa que simule el llenado de un álbum de figuritas
+y calcule el promedio entre múltiples personas. 
+'''
---
+++ PensamientoComputacional/ejercicios/ejercicios04.py
@@ -0,0 +1,8 @@
+def simularEvento():
+    return 'La probabilidad de que llenes el álbum es de x%'
+
+
+"""Establecer un valor máximo para la cantidad de figuritas que el usuario puede comprar (11): 
+Para promover la práctica de la lógica condicional, se propuso limitar la cantidad máxima de figuritas
+que un usuario puede comprar en una sola transacción. Esta restricción fomenta la planificación y toma
+de decisiones en el diseño del programa."""
