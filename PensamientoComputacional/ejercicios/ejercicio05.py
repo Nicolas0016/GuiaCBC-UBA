1318dbdae72084a0a9e5fe4cb2e693b408dd39cd
Nicolas <arganaraznicolas68@gmail.com>
2024-04-20 18:37:01 -0300

    add enunciado ejercicio 05

--- PensamientoComputacional/ejercicios/ejercicio05.py
+++ PensamientoComputacional/ejercicios/ejercicio05.py
@@ -1,13 +1,13 @@
 def resultadosJugadores():
     return "Resultados de los jugadores"
 
-# VOLVEMOS AL 7 y medio
-# El mazo es uno español (sin 8 ni 9)
-# Las cartas de 1 al 7 tienn su mismo valor
-# Las figuras tienen un valor de 0.5 (10 - 12)
-# Los jugadores tienen que estar más cerca del 7.5
-
-# Jugar varias rondas
-# hacer que los jugadores tengan diferentes estrategias
-# guardar todos los resultados de los jugadores
-# Mostrar el porsentaje de cada estrategia de juego
+# ¡Volvemos al 7 y medio!
+# Se utiliza un mazo español, excluyendo las cartas 8 y 9.
+# Las cartas del 1 al 7 tienen el mismo valor nominal.
+# Las figuras (10, 11 y 12) valen 0.5.
+# El objetivo es que los jugadores se acerquen lo más posible a 7.5.
+# Instrucciones:
+# Juega varias rondas del juego.
+# Cada jugador puede emplear diferentes estrategias.
+# Registra todos los resultados de los jugadores.
+# Muestra el porcentaje de uso de cada estrategia de juego.
